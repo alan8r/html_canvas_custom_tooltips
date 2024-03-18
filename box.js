da10e1f9d281adb9f31b9d090638362ad2aa6b04
alan8r <alan8r@gmail.com>
2024-03-18 01:52:06 -0700

    small tweaks and code cleanup, removed debugging junk

--- box.js
+++ box.js
@@ -1,9 +1,11 @@
 class Box {
 
+    // class constants
     static DEFAULT_COLOR = "seagreen";
     static DEFAULT_HOVER_COLOR = "red";
     static DEFAULT_ISVISIBLE = true;
 
+    // class constructor. takes in location/dimensions, tooltip text and modifier options
     constructor(x, y, width, height, tooltipText, options = {}) {
         this.x = x;
         this.y = y;
@@ -15,65 +17,85 @@ class Box {
         this.showTooltip = false;
 
         if (this.options.isVisible === undefined) this.options.isVisible = Box.DEFAULT_ISVISIBLE;
+        if (this.options.color === undefined) this.options.color = this.color;
     }
 
     update(mouse) {
-        if (this.mouseHover(mouse) && this.options.isVisible) {
+        // if the box is visible and the mouse is hovering over the box
+        if (this.options.isVisible && this.mouseHover(mouse)) {
+
             this.color = this.options.hoverColor || Box.DEFAULT_HOVER_COLOR;
+
+            // set the tooltip text to visible
             this.showToolTip = true;
+
+            // if the box is clicked, and its the "first" click (so action doesnt repeat)
             if (this.mouseClicked(mouse) && !mouse.downComplete) {
-                console.log("Box clicked!");
+                console.log(this.options.color + " box clicked!");
                 mouse.downComplete = true;
             }
+
         } else {
             this.color = this.options.color || Box.DEFAULT_COLOR;
             this.showToolTip = false;
         }
     }
 
+    // returns true/false based on whether mouse is hovering over box
     mouseHover(mouse) {
         return (
             mouse.x >= this.x &&
-            mouse.x <= this.x + this.width &&
+            mouse.x < this.x + this.width &&
             mouse.y >= this.y &&
-            mouse.y <= this.y + this.height
+            mouse.y < this.y + this.height
         );
     }
 
+    // returns true/false based on whether mouse clicks on box
     mouseClicked(mouse) {
         return (
             mouse.downX >= this.x &&
-            mouse.downX <= this.x + this.width &&
+            mouse.downX < this.x + this.width &&
             mouse.downY >= this.y &&
-            mouse.downY <= this.y + this.height
+            mouse.downY < this.y + this.height
         );
     }
 
+    // box drawing logic, needs mouse as param for tooltip location
     render(ctx, mouse) {
+        // if the box isn't visible, dont draw it
         if (!this.options.isVisible) return;
 
+        // otherwise, set up the colors and draw it
         ctx.fillStyle = this.color;
         ctx.fillRect(this.x, this.y, this.width, this.height);
 
+        // once the box is drawn, check if the tooltip is visible
         if (this.showToolTip) {
+
+            // for handling linebreaks since ctx.fillText doesn't
             let textArr = this.tooltipText.split("\n");
+
+            // get the longest line of a multiline string, since this will determine the tooltip box width
             let longestStr = "";
             for (let str of textArr) {
                 if (str.length > longestStr.length) {
                     longestStr = str;
                 }
             }
+
+            // set up the font and get its drawing dimensions from the longest line of text
             ctx.font = "0.75rem monospace";
             let textDims = ctx.measureText(longestStr);
             let textW = Math.ceil(textDims.width);
             let textH = Math.ceil(textDims.actualBoundingBoxAscent + textDims.actualBoundingBoxDescent);
-            let ttBorder = 2;
             let ttMargin = 10 + textArr.length;
-            let mYOffset = mouse.y + 20 + textH;
 
+            // draw the box that goes behind the tooltip text
             ctx.fillStyle = "#999";
             ctx.fillRect(mouse.x - (ttMargin / 2), mouse.y + textH + (ttMargin / 2), textW + ttMargin, (textH * textArr.length) + ttMargin);
 
+            //draw the tooltip text. the for loop handles multiline strings
             ctx.fillStyle = "black";
             for (let i = 0; i < textArr.length; i++) {
                 ctx.fillText(textArr[i], mouse.x, 20 + mouse.y + textH + ((textH + 2) * i));
--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-console.debug = ()=>undefined;
-
 // canvas stuff
 let canvas = document.querySelector("canvas");
 let ctx = canvas.getContext("2d");
 
+
 // set of boxes to add to canvas
 let boxes = [
-    new Box(canvas.width / 2 - 50, canvas.height / 2 - 50, 200, 100, "Big box."),
-    new Box(100, 100, 100, 50, "Small box!", { color: "blue" }),
+    new Box(canvas.width / 2 - 80, canvas.height / 2 - 80, 160, 160, "Big box."),
+    new Box(80, 100, 100, 50, "Small box!", { color: "#00f" }),
     new Box(160, 380, 50, 50, "This box shouldn't be visible.", { color: "orange", isVisible: false }),
-    new Box(380, 380, 25, 50, "This box should be visible,\nunlike the other orange box.\nIt also handles multiline decently.", { color: "orange" }),
+    new Box(450, 380, 25, 50, "This box should be visible,\nunlike the other orange box.\nIt also handles multiline decently.", { color: "orange" }),
 ];
 
 // mouse handler stuff
@@ -22,18 +21,17 @@ let mouse = {
     downComplete: false,
 }
 
+// set up all the event listeners for mouse stuff
 canvas.addEventListener("mousemove", (e) => {
-    mouse.x = e.layerX;
-    mouse.y = e.layerY;
+    let bounds = canvas.getBoundingClientRect();
+    [mouse.x, mouse.y] = [e.clientX - bounds.left, e.clientY - bounds.top];
 });
 
 canvas.addEventListener("mousedown", (e) => {
-    console.debug("### mousedown @", mouse.x, mouse.y);
     [mouse.downX, mouse.downY, mouse.down] = [mouse.x, mouse.y, true];
 });
 
 canvas.addEventListener("mouseup", (e) => {
-    console.debug("### mouseup @", mouse.x, mouse.y);
     [mouse.downX, mouse.downY, mouse.down, mouse.downComplete] = [-1, -1, false, false];
 });
 
@@ -44,13 +42,13 @@ function loop() {
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = "black";
     ctx.font = "2rem monospace";
-    ctx.fillText("Hover over boxes to see tooltips",20,40)
+    ctx.fillText("Hover over boxes to see tooltips!", 20, 40);
 
-    for (let box of boxes) {
-        console.debug("### tick", new Date().toTimeString().split(" ")[0]);
+    for (box of boxes) {
         box.update(mouse);
         box.render(ctx, mouse);
     }
+
     requestAnimationFrame(loop);
 }
 
